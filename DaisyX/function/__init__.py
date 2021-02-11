7b768a1327398cd4de32925b192e2cd216f62cc9
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-11 18:59:17 +0530

    Update __init__.py

--- DaisyX/function/__init__.py
+++ DaisyX/function/__init__.py
@@ -68,6 +68,47 @@ session = aiohttp.ClientSession()
 
 sedpath = TMP_DOWNLOAD_DIRECTORY
 
+async def fetch_json(link):
+    async with session.get(link) as resp:
+        return await resp.json()
+    
+
+
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    """Generic progress_callback for uploads and downloads."""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            try:
+                await event.edit(
+                    "{}\n**File Name:** `{}`\n{}".format(type_of_ps, file_name, tmp)
+                    
+                )
+            except:
+                pass
+        else:
+            try:
+                await event.edit("{}\n{}".format(type_of_ps, tmp))
+            except:
+                pass
+
+
 
 async def convert_to_image(event, borg):
     lmao = await event.get_reply_message()
