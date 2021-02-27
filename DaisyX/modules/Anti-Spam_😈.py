a9be6aa8708ccfb55d8e42a848c49c97f6566e9c
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-27 18:20:14 +0530

    Hmm

--- DaisyX/modules/Anti-Spam_😈.py
+++ DaisyX/modules/Anti-Spam_😈.py
@@ -61,3 +61,11 @@ __help__ = """
 """
 
 __mod_name__ = "Captcha ♞"
+
+__help__ = """
+ - /img2text <lang>: Type in reply to a image to extract the text from it
+ - /img2textlang: List all the available languages
+"""
+
+
+__mod_name__ = "Image2Text 📷"
