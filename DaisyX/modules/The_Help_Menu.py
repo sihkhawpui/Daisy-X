23b12e0d4eaf113521e2d0bc736f0c89c2074e5a
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-27 18:36:30 +0530

    Update The_Help_Menu.py

--- DaisyX/modules/The_Help_Menu.py
+++ DaisyX/modules/The_Help_Menu.py
@@ -109,3 +109,11 @@ If you are still messed up send `/helptorrent` in pm for the tutorial !
 """
 
 __mod_name__ = "Torrent 🌀"
+
+__help__ = """
+ - /weather <city>: Get weather info in a particular place
+ - /wttr <city>: Advanced weather module, usage same as /weather
+ - /wttr moon: Get the current status of moon
+"""
+
+__mod_name__ = "Weather ⛈️"
