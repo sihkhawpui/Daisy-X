bfdc7cde549a9e4a593e73742b705df2c9b190d4
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-27 18:35:08 +0530

    Update The_Help_Menu.py

--- DaisyX/modules/The_Help_Menu.py
+++ DaisyX/modules/The_Help_Menu.py
@@ -94,3 +94,11 @@ __help__ = """
  - /phone <tp no> : Track Phone no
 """
 __mod_name__ = "More Tools 🧩"
+
+__help__ = """
+ - /news <country code> <LanguageCode>: Returns today's American News Headlines (ONLY WORKS IN PM)
+**Example:**
+ - /news US en: This will return news for US in english language.
+"""
+
+__mod_name__ = "News 🗞️"
