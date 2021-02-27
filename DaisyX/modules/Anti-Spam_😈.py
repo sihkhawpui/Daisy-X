d801a7c8f791bbd901a0661ad4569ddf6d77cef3
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-27 18:19:37 +0530

    Merge

--- DaisyX/modules/Anti-Spam_😈.py
+++ DaisyX/modules/Anti-Spam_😈.py
@@ -36,3 +36,28 @@ If you are still messed up send `/helpcamscanner` in pm for the tutorial !
 """
 
 __mod_name__ = "CamScanner 🖨️"
+
+__help__ = """
+**New Welcome System**
+**☆ With chapta ☆**
+** ⚠ Older welcome system should be turned off in order to use this ⚠
+`/welcome off` and `/goodbye off` ※ ** To turn off** ※
+** ◇■ If not both systems will reply ■◇ **
+** Welcome **
+ - /savewelcome <welcome message> or <reply to a text>: Saves the message as a welcome note in the chat.
+ - /checkwelcome: Check whether you have a welcome note in the chat.
+ - /clearwelcome: Deletes the welcome note for the current chat.
+ - /welcomecaptcha <on/off>: Mutes a user on joining and unmutes as he/she solves a image captcha.
+ - /autocleanwelcome <on/off>: Clean previous welcome message before welcoming a new user
+**Goodbye**
+ - /savegoodbye <goodbye message> or <reply to a text>: Saves the message as a goodbye note in the chat.
+ - /checkgoodbye: Check whether you have a goodbye note in the chat.
+ - /cleargoodbye: Deletes the goodbye note for the current chat.
+ - /autocleangoodbye <on/off>: Clean previous goodbye message before farewelling a new user
+**Available variables for formatting greeting message:**
+`{mention}, {title}, {count}, {first}, {last}, {fullname}, {userid}, {username}, {my_first}, {my_fullname}, {my_last}, {my_mention}, {my_username}`
+**Note**: __You can't set new welcome/goodbye message before deleting the current one__
+⚠ READ FROM THE TOP ⚠
+"""
+
+__mod_name__ = "Captcha ♞"
