54a896612ad94dc7101bdef87fd2f8982d9477a3
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-27 18:35:53 +0530

    Update The_Help_Menu.py

--- DaisyX/modules/The_Help_Menu.py
+++ DaisyX/modules/The_Help_Menu.py
@@ -102,3 +102,10 @@ __help__ = """
 """
 
 __mod_name__ = "News 🗞️"
+
+__help__ = """
+ - /torrent <text>: Search for torrent links
+If you are still messed up send `/helptorrent` in pm for the tutorial !
+"""
+
+__mod_name__ = "Torrent 🌀"
