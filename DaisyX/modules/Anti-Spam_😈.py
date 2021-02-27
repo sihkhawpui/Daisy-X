bdb1d2cca6d873fae5d544ac40806fd188fef375
Inuka ASiTH <inukaasith7@gmail.com>
2021-02-27 18:18:39 +0530

    merge

--- DaisyX/modules/Anti-Spam_😈.py
+++ DaisyX/modules/Anti-Spam_😈.py
@@ -22,3 +22,17 @@ The question should be a meaningful one otherwise you will get no response !
 """
 
 __mod_name__ = "Voice AI 🤗"
+
+__help__ = """
+ - /camscanner: Reply to a image to scan and improve it's clarity.
+**Instructions**
+▪️The image should be a page with some written text on it (screenshots aren't permitted)
+▪️The image should contain the page with four corners clearly visible
+▪️The background should be somewhat darker than the page
+▪️The image should contain only the page with no other objects like pencil, eraser etc. beside it(within the image)
+**PRO TIP**
+You can simply draw a border(a black square) around the portion you want to scan for better efficiency and edge detection
+If you are still messed up send `/helpcamscanner` in pm for the tutorial !
+"""
+
+__mod_name__ = "CamScanner 🖨️"
